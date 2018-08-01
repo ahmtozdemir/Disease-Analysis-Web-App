6c6299ca3ae4b99dab8960e1694236d1b4576fb1
Ahmet Özdemir <32216376+ahmtozdemir@users.noreply.github.com>
2018-08-01 20:20:49 +0300

    Apriori Algorithm

--- src/servlet/ControllerServlet.java
+++ src/servlet/ControllerServlet.java
@@ -16,15 +16,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+
 @SuppressWarnings("Duplicates")
 @WebServlet(urlPatterns = "/ControllerServlet")
 public class ControllerServlet extends HttpServlet {
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String command=request.getParameter("command");
-        if(command.equals("birliktelik"))
-        {
+        if(command.equals("birliktelik")) {
             List<String> birliktelikListesi =new ArrayList<>();
             String hastalik = request.getParameter("hastalik");
             birliktelikListesi = birliktelikGetir(hastalik);
@@ -33,8 +32,7 @@ public class ControllerServlet extends HttpServlet {
             System.out.println("Dizimiz :" + birliktelikListesi);
             requestDispatcher.forward(request, response);
         }
-        else if(command.equals("enCok"))
-        {
+        else if(command.equals("enCok")) {
             List<String> enCokRastlananListesi=new ArrayList<>();
             enCokRastlananListesi=enCokRastlananHastalikKayitlariGetir();
             RequestDispatcher requestDispatcher = request.getRequestDispatcher("/enCokRastlanan.jsp");
@@ -44,10 +42,7 @@ public class ControllerServlet extends HttpServlet {
         }
     }
 
-
-
-    private List<String> birliktelikGetir(String hastalik) throws IOException 
-    {
+    private List<String> birliktelikGetir(String hastalik) throws IOException {
         String deger=hastalik+",";
         System.out.println( lineNumbers(deger) );
 
@@ -55,58 +50,39 @@ public class ControllerServlet extends HttpServlet {
         sayilar=lineNumbers(deger);
 
         List<String>sonrakiler=new ArrayList<>();
-        for(Integer i:sayilar)
-        {
-
-
-            String line = Files.readAllLines(Paths.get("C:\\Users\\Mevlana\\Intellij Projeler\\Hastalik-Analiz\\web\\data\\AssociationRules.txt")).get(i-1);
-
-
-            //Line � �nce 2 ye b�l�p sonra tekrar ikiye b�l�yoruz
+        for(Integer i:sayilar) {
+            String line = Files.readAllLines(Paths.get("C:\\Users\\Ahmet\\Intellij Projects\\Disease-Analysis\\web\\data\\AssociationRules.txt")).get(i-1);
             String[] dizi=line.split(":");
-            if(dizi[0].equals(deger))
-            {
+            if(dizi[0].equals(deger)) {
                 System.out.println(line);
-
                 sonrakiler.add(dizi[1]);
-
             }
-
-
         }
+        
         System.out.println("Fonksiyondan gelen dizi : "+sonrakiler);
         return sonrakiler;
     }
 
 
-    public static List<Integer> lineNumbers(String x) throws IOException
-    {
-        LineNumberReader lineReader = new LineNumberReader(new FileReader("C:\\Users\\Mevlana\\Intellij Projeler\\Hastalik-Analiz\\web\\data\\AssociationRules.txt"));
+    public static List<Integer> lineNumbers(String x) throws IOException {
+        LineNumberReader lineReader = new LineNumberReader(new FileReader("C:\\Users\\Ahmet\\Intellij Projects\\Disease-Analysis\\web\\data\\AssociationRules.txt"));
         List<Integer>sayilar=new ArrayList<>();
         String line;
-        while ((line = lineReader.readLine()) != null)
-        {
-            if (line.contains(x))
-            {
-
+        while ((line = lineReader.readLine()) != null) {
+            if (line.contains(x)) {
                 sayilar.add(lineReader.getLineNumber());
-
             }
         }
         return sayilar;
     }
 
-
-    private List<String> enCokRastlananHastalikKayitlariGetir() throws IOException
-    {
+    private List<String> enCokRastlananHastalikKayitlariGetir() throws IOException {
         List<String>dizi=new ArrayList<>();
-        LineNumberReader lineReader = new LineNumberReader(new FileReader("C:\\Users\\Mevlana\\Intellij Projeler\\Hastalik-Analiz\\web\\data\\frequentItemsets.txt"));
+        LineNumberReader lineReader = new LineNumberReader(new FileReader("C:\\Users\\Ahmet\\Intellij Projects\\Disease-Analysis\\web\\data\\frequentItemsets.txt"));
         String line;
-        while ((line = lineReader.readLine()) != null)
-        {
+        while ((line = lineReader.readLine()) != null) {
             dizi.add(line);
         }
         return  dizi;
     }
-
 }
